e71ea38aa4d04a69c5ea44bd529adb4cfa4e0a3f
Matt McCarthy <contact@mattmccarthy.net>
2020-12-10 14:07:24 -0500

    did person exercises

---
+++ Ch22/Person.hs
@@ -0,0 +1,53 @@
+module Ch22.Person where
+
+import Ch22.Reader
+import Control.Applicative
+
+newtype HumanName =
+  HumanName String
+  deriving (Eq, Show)
+
+newtype DogName =
+  DogName String
+  deriving (Eq, Show)
+
+newtype Address =
+  Address String
+  deriving (Eq, Show)
+
+data Person =
+  Person {
+    humanName :: HumanName
+  , dogName   :: DogName
+  , address   :: Address
+  } deriving (Eq, Show)
+
+data Dog =
+  Dog {
+    dogsName :: DogName
+  , dogsAddress :: Address
+  } deriving (Eq, Show)
+
+getDog :: Person -> Dog
+getDog p =
+  Dog (dogName p) (address p)
+
+getDogR :: Person -> Dog
+getDogR =
+  Dog <$> dogName <*> address
+
+getDogR' :: Person -> Dog
+getDogR' = liftA2 Dog dogName address
+
+getDogRM :: Reader Person Dog
+getDogRM = Reader $ getDogR'
+
+myLiftA2 :: Applicative f
+        => (a -> b -> c)
+        -> f a
+        -> f b
+        -> f c
+myLiftA2 f a b = f <$> a <*> b
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader f
